dc8a72deb771b7a8128181fbca9d4cc44b77ac98
Tony Minh-Hoang <hoangtrongminh304@gmail.com>
2016-06-28 10:22:22 +0700

    [Console-2][Chap21] Threading

---
+++ Chap21_Threading/CreateThread.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace Chap21_Thread
+{
+    class CreateThread
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thread Start/Stop/Join Sample");
+           
+            Thread oThread = new Thread(new ThreadStart(Beta));
+
+            // Start the thread
+            oThread.Start();
+
+            // Spin for a while waiting for the started thread to become alive
+            while (!oThread.IsAlive) ;
+
+            // Put the Main thread to sleep for 1 millisecond to allow oThread to do some work
+            Thread.Sleep(2);
+
+            // Request that oThread be stopped
+            oThread.Abort();
+
+            // Wait until oThread finishes. Join also has overloads that take a milisecond interval or a TimeSpan object.
+            oThread.Join();
+
+            Console.WriteLine();
+            Console.WriteLine("Beta has finished.");
+
+            try
+            {
+                Console.WriteLine("Try to restart the Beta thread");
+                oThread.Start();
+            }
+            catch (ThreadStateException)
+            {
+                Console.WriteLine("ThreadStateException trying to restart Beta.");
+                Console.WriteLine("Expected since aborted threads cannot be restarted.");
+                
+            }
+
+            //Console.ReadKey();
+        }
+
+        public static void Beta()
+        {
+            while (true)
+            {
+                Console.WriteLine("Beta is running in its own thread.");
+            }
+        }
+
+    }
+}
