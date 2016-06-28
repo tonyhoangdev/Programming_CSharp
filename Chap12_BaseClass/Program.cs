4e09e1aa5c713539629bc4f11bda727fcf134e65
Tony Minh-Hoang <hoangtrongminh304@gmail.com>
2016-06-28 10:41:52 +0700

    [Console][Chap12] Add example for base class

---
+++ Chap12_BaseClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Chap12_BaseClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer();
+
+            // khai bao ham xu ly
+            myTimer.Elapsed += new ElapsedEventHandler(DisplayTimeEvent);
+            // khoang thoi gian delay
+            myTimer.Interval = 1000;
+            myTimer.Start();
+
+            while(Console.Read() != 'q')
+            {
+                ;
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void DisplayTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("\n{0}", DateTime.Now);
+        }
+    }
+}
---
+++ Chap12_BaseClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Chap12_BaseClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chap12_BaseClass")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30ccf05c-c4cf-41d9-8b7a-379c5bc5891a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
